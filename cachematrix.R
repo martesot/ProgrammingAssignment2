d4e888ecc9bf2b20bcff4d3c0d5051914cccd880
martesot <106917484+martesot@users.noreply.github.com>
2022-06-06 08:33:59 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,39 +3,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) 
+makeCacheMatrix <- function(x = matrix())  ## The given code for function
         {
- inv <- NULL
- set <- function(y) 
+ inv <- NULL    ## Set value to 0, to initialize the inverse matrix as null
+ set <- function(y)     
    {
    x <<- y
    inv <<- NULL
  }
-  get <- function() x
+  get <- function() x   ## Function to get Matrix
   setinv <- function(inverse) inv <<- inverse
   getinv <- function() inv 
   {
-    inver <<- ginver(x)
-    inver%%x
+    inver <<- ginver(x)         ## 
+    inver%%x    ## Function to get inverse of matrix
   }
   list(set = set, get = get,
        setinv = setinv,
-       getinv = getinv)
+       getinv = getinv)         ## Defines the list
 }
 
 
-## Write a short comment describing this function
+## Have now defined a matrix
+## The next function finds the inverse of the matrix. 
+## First it checks whether the inverse has already been given, and if so it skips. 
+## If not, it gives the cache by the «set inverse» function.
 
-cacheSolve <- function(x, ...) 
+cacheSolve <- function(x, ...)  ## Code given to describe/solve this assignment
         {
   inv <- x$getinv()
   if(!is.null(inv)) 
     {
     message("cached")
-    return(inv)
+    return(inv)         ## Returns value of inverse
   }
   data <- x$get()
   inv <- solve(data, ...)
   x$setinv(inv)
-  inv
+  inv   ## Returns matrix that is inverse of x
 }
+
+## Can further test if this actually works by doing a test with real numbers
+
+myFunction <- makeCacheMatrix(matrix(1:4, 3, 2))
+myFunction$get()
+myFunction$getinverse()
+cacheSolve(myFunction)
